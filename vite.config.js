c3e3042e408380c62a90ee6fc2c4a931f09418eb
Doug Franklin <doug@dougfranklin.com>
2022-06-27 12:36:08 -0400

    Icon Works, SQLite doesn't

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,28 @@ const path = require("path");
 // https://vitejs.dev/config/
 export default defineConfig({
   base: path.resolve(__dirname, "./dist/"),
-  plugins: [vue()]
+  plugins: [vue()],
+  pluginOptions: {
+    electronBuilder: {
+        preload: 'src/preload.js',
+      builderOptions: {
+        extraResources: ['src', 'src/res/'],
+      },
+      externals: ['sqlite3'],
+    },
+  },
 })
+
+// vue.config.js
+// module.exports = {
+//   transpileDependencies: ['vuetify'],
+//   pluginOptions: {
+//     electronBuilder: {
+//         preload: 'src/preload.js',
+//       builderOptions: {
+//         extraResources: ['src', 'src/res/'],
+//       },
+//       externals: ['knex', 'sqlite3'],
+//     },
+//   },
+// };
